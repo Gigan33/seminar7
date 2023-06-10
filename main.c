51930147d9a93db96f1dbef33edcabbecd4d847d
Gigan33 <Reutov250A@yandex.ru>
2023-06-10 12:16:49 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void shellSort(int arr[], int n) {
+    int gap = n / 2;
+    while (gap > 0) {
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
+        gap /= 2;
+    }
+}
+
+
+int main() {
+    int lenth;
+
+    puts("Введите количество чисел в массиве:");
+    scanf("%d", &lenth);
+
+    int *arr = (int*) malloc(lenth * sizeof(int));
+
+    puts("Введите эти числа:");
+    for(int i = 0; i < lenth; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    shellSort(arr, lenth);
+
+    for (int i = 0; i < lenth; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    free(arr);
+    return 0;
+}
